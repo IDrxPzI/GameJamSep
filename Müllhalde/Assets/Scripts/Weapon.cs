ec452ea975db4c82d19ab69f8e1d8d28dd61a0fc
IDrxPzI <stefan.mench@web.de>
2023-09-15 17:28:08 +0200

    .

--- Müllhalde/Assets/Scripts/Weapon.cs
+++ Müllhalde/Assets/Scripts/Weapon.cs
@@ -1,8 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Weapon : MonoBehaviour
 {
-    Debug.DrawRay
+    [SerializeField] private GameObject bulletPrefab;
+
+    private void Update()
+    {
+        Debug.DrawLine(transform.position, new Vector3(0, 0, 100), Color.green);
+    }
+
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+        }
+    }
 }
