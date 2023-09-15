c8bcbaa397f82598aa9307bbcf5aa236e796aa64
INTingKING <82975137+INTingKING@users.noreply.github.com>
2023-09-15 23:16:19 +0200

    useless bastard slimes which go spidget finner fucking ass fuck fuxk your mother you'd rather annoy me than going to your mom lying in her death bed bastard

--- Müllhalde/Assets/Scripts/Enemy_Slime.cs
+++ Müllhalde/Assets/Scripts/Enemy_Slime.cs
@@ -8,21 +8,23 @@ public class Enemy_Slime : MonoBehaviour
 {
     public GameObject player;
 
-    public float slimeAvoidSpeed = 1;
+    public float slimeAvoidSpeed = 1f;
 
     private NavMeshAgent navMeshAgent;
+    private Rigidbody rb;
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         navMeshAgent.SetDestination(player.transform.position);
     }
@@ -30,16 +32,10 @@ public class Enemy_Slime : MonoBehaviour
     private void OnTriggerEnter(Collider enemy)
     {
         if (enemy.gameObject.tag != "Enemy") return;
-        UnityEngine.Vector3 avoidDirection = Vector3.Minus(
-            transform.position, 
-            enemy.gameObject.transform.position
-        );
-
-        transform.position = new UnityEngine.Vector3(
-            avoidDirection.x*slimeAvoidSpeed*Time.deltaTime, 
-            0, 
-            avoidDirection.z*slimeAvoidSpeed*Time.deltaTime
-        );
+        Vector3 avoidDirection = enemy.gameObject.transform.position - transform.position;
+        avoidDirection.Normalize();
+        rb.velocity = new Vector3(avoidDirection.x * slimeAvoidSpeed*-1, 0f, avoidDirection.z* slimeAvoidSpeed);
+        Debug.Log(avoidDirection);
     }
 }
 
